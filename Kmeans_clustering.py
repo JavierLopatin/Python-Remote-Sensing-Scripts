1385e68e42444ef685ac49c0db778c331fcc3719
javier lopatin <javierlopatin@gmail.com>
2018-11-20 19:02:50 -0800

    KMeans clustering of S-1 and -2 data

--- Kmeans_clustering.py
+++ Kmeans_clustering.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-
 K-means clustering using Sentinel-1 and -2 data
 
-Use the SentinelHub package and account to download atmospherically
-corrected images directly to the python session
+- Use the SentinelHub package and account to download atmospherically
+  corrected Sentinel-2 and Sentinel-1 to the python session.
+
+- Apply K-Mean clustering in iterative mode. Select the final number of
+  clusters accordingly to the silhouette score.
+
+- Apply pre-processing functions, such as PCA transformation and Brightness
+  Normalization.
 
 Created on Thu Aug 16 14:26:23 2018
 
@@ -19,7 +24,7 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from sentinelhub import WmsRequest, MimeType, CRS, BBox, CustomUrlParam, geo_utils, DataSource
 
-
+################################
 ### functions
 
 def plot_image(image, factor=1):
@@ -39,21 +44,23 @@ def run_Kmeans(raster, numSamples=10000, bnorm=False, pca=False, n_jobs=-2):
     select n number of pixeles to perform the clustering analysis
     first, reshape image to be (rows*columns, bands)
 
-    Optional preprocessing options:
-        - bnorm: Brightness normalization
-        - pca: principal component analysis
+    Parameters:
+    - numSamples: number of random samples taken to train the cluster. Default = 10000
+    - bnorm: apply Brightness normalization to the data
+    - pca: apply principal component transformation to the data
+    - n_jobs: number of CPU cores to use. Default: all - 2
 
     """
     X = np.reshape(raster, (rows*columns, raster.shape[2]))
 
-    # brightness normalization
+    # if brightness normalization
     if bnorm == True:
         def norm(r):
             norm = r / np.sqrt( np.sum((r**2), 0) )
             return norm
         X = np.apply_along_axis(norm, 0, X)
 
-    # pca
+    # if pca
     if pca == True:
         X = PCA(n_components=X.shape[1]).fit_transform(X)
 
@@ -76,11 +83,12 @@ def run_Kmeans(raster, numSamples=10000, bnorm=False, pca=False, n_jobs=-2):
 
     """
 
-    Stop!!! ask for the best n_Clusters to be used
+    Stop!!! ask for the best n_Clusters to be used. Enter the number in the terminal
 
     """
     bestCluster = input("Please enter your selected number of clusters: ")
 
+    # run KMeans one last time using the selected number of clusters
     kmeans = KMeans(n_clusters = int(bestCluster), random_state=123, n_jobs=n_jobs)
     cluster_labels = kmeans.fit_predict(samples)
     silhouette = silhouette_score(samples, cluster_labels)
@@ -89,23 +97,24 @@ def run_Kmeans(raster, numSamples=10000, bnorm=False, pca=False, n_jobs=-2):
     # reshape prediction to shape (rows, columns)
     predCluster = np.reshape(predCluster, (rows, columns))
 
-    # save kmeans clustering
+    # save kmeans clustering as:
     output = "Sentinel_40m_kmeans_"+bestCluster+"k.tif"
-
     if bnorm == True:
         output = output[:-4] + "_bnorm.tif"
     if pca == True:
         output = output[:-4] + "_pca.tif"
 
-    # Update meta to reflect the number of layers ad dtype
+    # Update meta to reflect the number of layers; add dtype
     meta.update(dtype=str(predCluster.dtype), count = 1)
 
-    # save
+    # save cluster to disk
     with rasterio.open(output, 'w', **meta) as dst:
         dst.write(predCluster, 1)
 
     return(predCluster)
 
+######################################
+### END FUNCTIONS
 
 if __name__ == "__main__":
 
@@ -113,9 +122,11 @@ if __name__ == "__main__":
     """
 
     Load Sentinel-1 and -1 images from SentinelHub
+    We selected 20m of spatial resolution for speed.
 
     """
 
+    # this is a personal code number. You need to add your own to work
     INSTANCE_ID = '7748e676-f55c-4843-900b-1d3e2962a7f1'
 
     # define study area.
@@ -129,27 +140,28 @@ if __name__ == "__main__":
     """
     geo_utils.bbox_to_resolution(cauquenes_bbox, 1570, 1600)
 
+    ########################################################
+    ### Sentinel-2
 
-    # chack for the last available S-2 image with less than 30% cloud cover
+    # check for the last available S-2 image with less than 30% cloud cover
+    # only in RGB color for visualization
     wms_true_color_request = WmsRequest(layer='TRUE-COLOR-S2-L1C',
                                         bbox=cauquenes_bbox,
                                         time=('2017-12-01', '2017-12-31'),
                                         maxcc=0.2,
-                                        width=800, height=800,
+                                        width=3570, height=3600,
                                         instance_id=INSTANCE_ID)
-
-
+    # get all images to python session
     wms_true_color_img = wms_true_color_request.get_data()
 
     print('These %d images were taken on the following dates:' % len(wms_true_color_img))
     for index, date in enumerate(wms_true_color_request.get_dates()):
         print(' - image %d was taken on %s' % (index, date))
 
-
+    # see images one by one
     plot_image(wms_true_color_img[1])
 
-
-    # Download raw bands
+    # Download raw bands of the selected image with 20m pixel size
     wms_bands_request = WmsRequest(data_folder='test_dir_tiff',
                                    layer='BANDS-S2-L1C',
                                    bbox=cauquenes_bbox,
@@ -158,30 +170,34 @@ if __name__ == "__main__":
                                    image_format=MimeType.TIFF_d32f,
                                    instance_id=INSTANCE_ID,
                                    custom_url_params={CustomUrlParam.ATMFILTER: 'ATMCOR'})
-
+    # save image to disk just in case
     wms_bands_img = wms_bands_request.save_data()
-
+    # load aimage to python session
     wms_bands_img = wms_bands_request.get_data()
-
+    # see image
     plot_image(wms_bands_img[-1])
 
-    ### Sentinel-1 IW polyrization
+    ########################################################
+    ### Sentinel-2
+
+    ### Sentinel-1 IW polyrization. Request RGB for visualizaation
     s1_request = WmsRequest(data_source=DataSource.SENTINEL1_IW,
                             layer='TRUE-COLOR-S1-IW',
                             bbox=cauquenes_bbox,
                             time=('2017-12-05', '2017-12-15'),
                             width=1190, height=1200,
                             instance_id=INSTANCE_ID)
-
+    # get data to python session
     s1_data = s1_request.get_data()
 
     print('These %d images were taken on the following dates:' % len(s1_data))
     for index, date in enumerate(s1_request.get_dates()):
         print(' - image %d was taken on %s' % (index, date))
 
+    # plot images one by one
     plot_image(s1_data[2])
 
-    # dowload
+    # dowload raw bands of the selected image with 20m pixel size
     s1_request = WmsRequest(data_folder='test_dir_tiff',
                             data_source=DataSource.SENTINEL1_IW,
                             layer='BANDS-S1-IW',
@@ -190,12 +206,16 @@ if __name__ == "__main__":
                             width=1570, height=1600,
                             image_format=MimeType.TIFF_d32f,
                             instance_id=INSTANCE_ID)
+    # load data to python session
     s1_data = s1_request.save_data()
 
-    # import raster
+    #######################################################
+    ### Analysis
+
+    # import raster. You also can use directly the loaded images from SentinelHub
     S2 = "test_dir_tiff/S2_40m.tiff"
     S1 = "test_dir_tiff/S1_40m_IW.tiff"
-    shp = "shapefile.shp"
+    shp = "shapefile.shp" # study area to crop the images
 
     # load shapefile geometry
     with fiona.open(shp, "r") as shapefile:
@@ -204,26 +224,41 @@ if __name__ == "__main__":
     # open and crop S2
     with rasterio.open(S2) as src:
         img, transform = rasterio.mask.mask(src, features, crop=True)
-        meta = src.meta.copy()
-        meta.update({"driver": "GTiff",
+        meta = src.meta.copy() # save metadata info
+        meta.update({"driver": "GTiff", # update metadata geometry to the croped version
                      "height": img.shape[1],
                      "width": img.shape[2],
                      "transform": transform})
-        baseName = src.name
-        bands, rows, columns = img.shape
+        baseName = src.name # raster name
+        bands, rows, columns = img.shape # raster size
 
-    # open and crop S1
+    # open and crop S1. Metadata not needed, same specifications as S2
     with rasterio.open(S1) as src:
         img2, transform = rasterio.mask.mask(src, features, crop=True)
 
     # stack S2 and S1 images
     raster = np.concatenate((img, img2), axis=0)
-
+    # vosualize stacked raster
     plot_image(raster[:,:,[8,4,3]])
 
     # transpose raster to shape (rows, columns, bands)
     raster = np.transpose(raster, [1,2,0])
 
-    ### run K-means
-    kmeans = run_Kmeans(raster, bnorm=True, pca=True)
-    plot_image(kmeans)
+    ##############################################################
+    ### run K-means with differing pre-processing transformations
+
+    # KMeans with raw data
+    kmeans1 = run_Kmeans(raster)
+    plot_image(kmeans2)
+
+    # KMeans with PCA transformaiton
+    kmeans2 = run_Kmeans(raster, pca=True)
+    plot_image(kmeans2)
+
+    # KMeans with brightness normalization
+    kmeans3 = run_Kmeans(raster, bnorm=True)
+    plot_image(kmeans3)
+
+    # KMeans with brightness normalization and PCA transformaiton 
+    kmeans4 = run_Kmeans(raster, bnorm=True, pca=True)
+    plot_image(kmeans4)
