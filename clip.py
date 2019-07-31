0c5c5c95b76124ec358390b1bce72abe6df5c285
JavierLopatin <javierlopatin@gmail.com>
2019-07-31 13:30:26 +0200

    fix labels

--- clip.py
+++ clip.py
@@ -10,33 +10,44 @@ Created on Tue Oct 23 16:32:37 2018
 @author: Javier Lopatin | javier.lopatin@kit.edu
 """
 
-import rasterio, rasterio.mask, argparse
+import rasterio
+import rasterio.mask
+import fiona
+import argparse
 
 # create the arguments for the algorithm
 parser = argparse.ArgumentParser()
 
 # set arguments
-parser.add_argument('-r','--inputRaster', help='Input raster', type=str, required=True)
-parser.add_argument('-v','--inputVector', help='Input raster', type=str, required=True)
+parser.add_argument('-r', '--inputRaster', help='Input raster', type=str, required=True)
+parser.add_argument('-s', '--inputshape', help='Input shapefile', type=str, required=True)
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 args = vars(parser.parse_args())
 
 
 # set argument
 raster = args["inputRaster"]
-shp = args["inputVector"]
+shp = args["inputshape"]
+
+# load shapefile shapes
+with fiona.open(shp, "r") as shapefile:
+    features = [feature["geometry"] for feature in shapefile]
 
 # open and crop raster
 print("Clipping raster...")
 with rasterio.open(raster) as src:
-     img, transform = rasterio.mask.mask(src, shp, crop=True)
+     img, transform = rasterio.mask.mask(src, features, crop=True)
      meta = src.meta.copy()
-     baseName = src.name
+
+meta.update({"driver": "GTiff",
+             "height": img.shape[1],
+             "width": img.shape[2],
+             "transform": transform})
 
 # output name
-output = baseName[:-4] + "_mask.tif"
+output = raster[:-4] + "_mask.tif"
 
 # save
 with rasterio.open(output, 'w', **meta) as dst:
-    dst.write(img, 1)
+    dst.write(img)
 print("Done!")
