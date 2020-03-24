3a0d0d8e2d0ad614176ec2c44fa73394545a4843
JavierLopatin <javierlopatin@gmail.com>
2020-03-24 15:25:05 -0300

    add parallel processing

--- stratified_points_in_polygon.py
+++ stratified_points_in_polygon.py
@@ -15,7 +15,8 @@ from joblib import Parallel, delayed
 from tqdm import tqdm  # just for nice visualization
 
 
-def random_points_in_polygon(shp, number=100, dist=10, maxiter=1000, n_jobs=8):
+def random_points_in_polygon(shp, cassNames, number=100, dist=10,
+                             maxiter=1000, n_jobs=8):
     '''
     Generate random points inside polygons
 
@@ -33,7 +34,12 @@ def random_points_in_polygon(shp, number=100, dist=10, maxiter=1000, n_jobs=8):
             does not hold the selected 'number' of points due to 'dist'
 
     '''
-    def my_function(polygon):
+    # read shapefile and get labels
+    shp = gpd.read_file(shp)
+
+    def my_function(j, shp):
+        # ansilary funciton to be passed to the parallel processing
+        polygon = shp.geometry[j]
         points = []
         min_x, min_y, max_x, max_y = polygon.bounds
         i = 0
@@ -51,18 +57,17 @@ def random_points_in_polygon(shp, number=100, dist=10, maxiter=1000, n_jobs=8):
                             points.pop()
                     except i > maxiter:
                         sys.exit(1)
-                i += 1
-
-        return gpd.GeoDataFrame(crs=shp.crs, geometry=points)  # returns list of shapely point
-
-    myList = list(shp.geometry)
+            i += 1
+        df = gpd.GeoDataFrame(crs=shp.crs, geometry=points)
+        df['class'] = shp[cassNames][j]
+        return df
 
     # parallel processing through the list
-    num_cores = multiprocessing.cpu_count()-2
-    # Parallel(n_jobs=n_jobs)(delayed(funciton)(parateters of function) for polygon in tqdm(myList))
-    outlist = Parallel(n_jobs=n_jobs)(delayed(my_function)(polygon) for polygon in tqdm(myList))
+    num_cores = multiprocessing.cpu_count()
+    outlist = Parallel(n_jobs=num_cores)(delayed(my_function)(j, shp)
+                                         for j in tqdm(range(len(shp))))
 
-    return pd.concat(outlist)
+    return pd.concat(outlist)  # concatenate all data
 
 
 if __name__ == "__main__":
